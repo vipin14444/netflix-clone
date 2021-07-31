6feb8beeb78fad59466a77e33adad0d7c6879d80
vipin14444 <vipin.sharma3039@gmail.com>
2021-07-31 16:20:30 +0530

    animation on topbar

--- src/app/shared/topbar/topbar.component.ts
+++ src/app/shared/topbar/topbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent implements OnInit {
+  onTop: boolean = window.scrollY === 0 ? true : false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(): void {
+    let updatedOnTop: boolean;
+    updatedOnTop = window.scrollY === 0 ? true : false;
+    if (updatedOnTop !== this.onTop) {
+      this.onTop = updatedOnTop;
+      console.log(this.onTop);
+    }
   }
-
 }
